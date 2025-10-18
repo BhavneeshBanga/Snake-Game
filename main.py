2488770a2e6c783a266fc0de21a91af8622f7315
Bhavneesh <bhavibanga505@gmail.com>
2025-10-18 14:21:55 +0530

    moving right

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ game_over = False
 snake_x = 45
 snake_y = 45
 snake_size = 10
+fps = 30
+clock = pygame.time.Clock()
 
 
 #game loop
@@ -30,10 +32,15 @@ while not exit_game:
     for event in pygame.event.get():
         if(event.type == pygame.QUIT):
             exit_game = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                snake_x += 10
 
     gameWindow.fill(white)
     pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])
     pygame.display.update()
+    clock.tick(fps)
+
 
 pygame.quit()
 quit()
